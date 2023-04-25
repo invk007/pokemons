51228f79b9ab589cc8291cf9679f457d44cd4e5a
Ilya Novikau <ilya.nvu@gmail.com>
2023-04-25 23:49:23 +0300

    Remove unused methods

--- app/tools.py
+++ app/tools.py
@@ -1,29 +1,8 @@
 import json
-import time
 import typing as t
-from functools import wraps
 
 import requests
 from requests import HTTPError
-
-
-def timer(func: t.Callable) -> t.Callable:
-    """
-    Measure function running time.
-
-    :param func:
-    :return:
-    """
-
-    @wraps(func)
-    def inner(*args, **kwargs):
-        start = time.perf_counter()
-        result = func(*args, **kwargs)
-        end = time.perf_counter()
-        print(f'Function run time = {end - start}')
-        return result
-
-    return inner
 
 
 def get(url: str) -> t.Dict:
@@ -37,7 +16,7 @@ def get(url: str) -> t.Dict:
         response = requests.get(url)
     except HTTPError as err:
         raise Exception(f'Request to API failed with error: {err}')
-    except Exception as err:
+    except Exception:
         raise Exception('Unexpected error while getting data from API')
 
     return response.json()
