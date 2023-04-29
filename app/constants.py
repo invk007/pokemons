ccbfa93f3b42d0fa6f9a876282423d9b02df1bc5
Ilya Novikau <ilya.nvu@gmail.com>
2023-04-29 17:56:20 +0300

    Install opencv bindings

--- app/constants.py
+++
@@ -1,2 +0,0 @@
-API_URL = 'https://pokeapi.co/api/v2/pokemon'
-LIMIT = 200
--- app/loaders.py
+++ app/loaders.py
@@ -2,38 +2,39 @@ import asyncio
 import logging
 from concurrent.futures import ThreadPoolExecutor, as_completed
 
-from app import constants
-from app.tools import async_get, get, save_to_file
+from app.tools import async_get, get
 
 logging.basicConfig()
 _logger = logging.getLogger(__name__)
 _logger.setLevel(logging.INFO)
 
+API_URL = 'https://pokeapi.co/api/v2/pokemon'
+LIMIT = 200
 
-def dump_pokemons_threads():
+
+def dump_pokemons_threads() -> list[dict]:
     """
-    Retrieves a list of Pokemons from an external API using multiple threads,
-    and saves it to a file.
+    Retrieves a list of Pokemons from an external API using multiple threads
 
-    :return: None
+    :return: list of dicts with pokemons info
     :raises Exception: if there is an error while getting data from the API or
     writing it to file
     """
     try:
-        general = get(f'{constants.API_URL}?limit=1')
+        general = get(f'{API_URL}?limit=1')
     except Exception as err:
         _logger.exception(f'Failed to get information from API: {err}')
-        return
+        raise
 
     qty = general.get('count', 0)
-    offsets = [offset for offset in range(0, qty, constants.LIMIT)]
+    offsets = [offset for offset in range(0, qty, LIMIT)]
     pokemons = []
 
     with ThreadPoolExecutor(max_workers=4) as executor:
         futures = [
             executor.submit(
                 get,
-                f'{constants.API_URL}?offset={offset}&limit={constants.LIMIT}',
+                f'{API_URL}?offset={offset}&limit={LIMIT}',
             )
             for offset in offsets
         ]
@@ -49,30 +50,29 @@ def dump_pokemons_threads():
                     f'Task raised an exception: {err}', exc_info=True
                 )
 
-    save_to_file('pokemons_threads.json', pokemons)
+    return pokemons
 
 
-async def dump_pokemons_async():
+async def dump_pokemons_async() -> list[dict]:
     """
-    Retrieves a list of Pokemons from an external API asynchronously, and saves
-    it to a file.
+    Retrieves a list of Pokemons from an external API asynchronously
 
-    :return:
+    :return: list of dicts with pokemons info
     """
     pokemons = []
 
     try:
-        general = await async_get(f'{constants.API_URL}?limit=1')
+        general = await async_get(f'{API_URL}?limit=1')
     except Exception as err:
         _logger.exception(f'Failed to get information from the API: {err}')
-        return
+        raise
 
     qty = general.get('count', 0)
-    offsets = [offset for offset in range(0, qty, constants.LIMIT)]
+    offsets = [offset for offset in range(0, qty, LIMIT)]
 
     coroutines = [
         async_get(
-            f'{constants.API_URL}?offset={offset}&limit={constants.LIMIT}'
+            f'{API_URL}?offset={offset}&limit={LIMIT}'
         )
         for offset in offsets
     ]
@@ -89,21 +89,4 @@ async def dump_pokemons_async():
             f'Failed to load pokemons information: {err}', exc_info=True
         )
 
-    save_to_file('pokemons_async.json', pokemons)
-
-
-def dump_pokemons_seq():
-    """
-    Retrieves a list of Pokemons from an external API sequentially, and saves it
-    to a file.
-    :return:
-    """
-    pokemons = []
-    next_ = f'{constants.API_URL}?limit={constants.LIMIT}'
-
-    while next_:
-        result = get(next_)
-        pokemons += result.get('results', [])
-        next_ = result.get('next')
-
-    save_to_file('pokemons_seq.json', pokemons)
+    return pokemons
